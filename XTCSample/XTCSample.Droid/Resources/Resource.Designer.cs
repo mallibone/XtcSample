6f6f9ec91320d7efb8dbd65e8936007400dac79d
mallibone <mallibone@outlook.com>
2016-03-28 08:29:08 +0200

    Adds basic structure files

--- XTCSample/XTCSample.Droid/Resources/Resource.Designer.cs
+++ XTCSample/XTCSample.Droid/Resources/Resource.Designer.cs
@@ -26,6 +26,7 @@ namespace XTCSample.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::XTCSample.Droid.Resource.Attribute.actionBarSize;
 		}
 		
 		public partial class Animation
@@ -2454,6 +2455,12 @@ namespace XTCSample.Droid
 		
 		public partial class String
 		{
+			
+			// aapt resource value: 0x7f05001f
+			public const int ApplicationName = 2131034143;
+			
+			// aapt resource value: 0x7f05001e
+			public const int Hello = 2131034142;
 			
 			// aapt resource value: 0x7f05000b
 			public const int abc_action_bar_home_description = 2131034123;
---
+++ XTCSample/XTCSample/Locator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+using XTCSample.Services.Person;
+using XTCSample.Services.Person.Impl;
+using XTCSample.ViewModels;
+
+namespace XTCSample
+{
+    internal class Locator
+    {
+        /// <summary>
+        /// Register all the used ViewModels, Services et. al. witht the IoC Container
+        /// </summary>
+        public Locator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            // ViewModels
+            SimpleIoc.Default.Register<MainViewModel>();
+            SimpleIoc.Default.Register<DetailViewModel>();
+
+            // Services
+            SimpleIoc.Default.Register<IPeopleService, PeopleServiceStub>();
+        }
+
+        /// <summary>
+        /// Gets the Main property.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance",
+            "CA1822:MarkMembersAsStatic",
+            Justification = "This non-static member is needed for data binding purposes.")]
+        public MainViewModel Main
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MainViewModel>();
+            }
+        }
+
+        /// <summary>
+        /// Gets the Detail property.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance",
+            "CA1822:MarkMembersAsStatic",
+            Justification = "This non-static member is needed for data binding purposes.")]
+        public DetailViewModel Detail
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<DetailViewModel>();
+            }
+        }
+    }
+}
