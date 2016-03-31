96abd104b2e9252291a67d8b0166c9c7f44296bd
Mark Allibone <mark.allibone@noser.com>
2016-03-31 08:07:36 +0200

    Adds person edit functionality

--- XTCSample/XTCSample/Models/Person.cs
+++ XTCSample/XTCSample/Models/Person.cs
@@ -1,15 +1,20 @@
+using System;
+
 namespace XTCSample.Models
 {
     public class Person
     {
         public Person(string firstName, string lastName)
         {
+            if (firstName == null) throw new ArgumentNullException(nameof(firstName));
+            if (lastName == null) throw new ArgumentNullException(nameof(lastName));
             FirstName = firstName;
             LastName = lastName;
         }
 
-        public string FirstName { get; private set; }
-        public string LastName { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string FullName { get { return FirstName + " " + LastName; } }
+        public static Person Null { get; } = new Person(string.Empty, string.Empty);
     }
 }
--- XTCSample/XTCSample/ViewModels/DetailViewModel.cs
+++ XTCSample/XTCSample/ViewModels/DetailViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GalaSoft.MvvmLight;
-using XTCSample.Models;
+﻿using XTCSample.Models;
 
 namespace XTCSample.ViewModels
 {
@@ -17,14 +11,34 @@ namespace XTCSample.ViewModels
             get { return _selectedPerson; }
             set
             {
+                if(value == null || value ==_selectedPerson) return;
                 _selectedPerson = value;
                 RaisePropertyChanged(nameof(FirstName));
                 RaisePropertyChanged(nameof(LastName));
             }
         }
 
-        public string FirstName => SelectedPerson.FirstName;
-        public string LastName => SelectedPerson.LastName;
+        public string FirstName
+        {
+            get { return SelectedPerson.FirstName; }
+            set
+            {
+                if(value == SelectedPerson.FirstName) return;
+                SelectedPerson.FirstName = value;
+                RaisePropertyChanged(nameof(FirstName));
+            }
+        }
+
+        public string LastName
+        {
+            get { return SelectedPerson.LastName; }
+            set
+            {
+                if (value == SelectedPerson.LastName) return;
+                SelectedPerson.LastName = value;
+                RaisePropertyChanged(nameof(LastName));
+            }
+        }
 
         public void Init(Person selectedPerson)
         {
